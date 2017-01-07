646759b6bcb7a91ccc25a2d0a08ee8af15fc7aa4
jasoncrider <xylude@gmail.com>
2017-01-07 09:51:37 -0500

    First commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    slack_token: "my-slack-token",
+    slack_token: "xoxb-123590449649-TINPn8YAHtT8ngDoTussyfua",
     channel_to_post_output_into: "#general" //channel you want the bot to post output to. Should be the same as the channel it joined
 }
