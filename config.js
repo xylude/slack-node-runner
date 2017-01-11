52f9de16a161213cd24b4f26056caa5cdc2f6d95
jasoncrider <xylude@gmail.com>
2017-01-11 12:31:49 -0500

    Removed my bot token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    slack_token: "xoxb-123590449649-TINPn8YAHtT8ngDoTussyfua",
+    slack_token: "my-bot-token",
     channel_to_post_output_into: "#general", //channel you want the bot to post output to. Should be the same as the channel it joined
     ignore_modules: ['fs','child_process','exec','os', 'cluster', 'http', 'https', 'net'] //modules to refuse to load
 }
